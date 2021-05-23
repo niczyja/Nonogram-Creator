6370ded5337eacf5c98550197b07c10668856cf8
Maciej Sienkiewicz <maciej@niczyja.pl>
2021-05-23 13:11:05 +0200

    Initial project structure

---
+++ Nonogram Creator/Nonogram Creator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nonogram Creator
+//
+//  Created by Maciej Sienkiewicz on 23/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
